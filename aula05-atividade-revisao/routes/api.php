9972437d10463288ff190ef7b4c84638bf07bf3c
Nando <FSNando.012@outlook.com>
2025-09-10 23:29:18 -0300

    Rotas atualizadas

--- aula05-atividade-revisao/routes/api.php
+++ aula05-atividade-revisao/routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\AlunoController;
+use App\Http\Controllers\CursoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/cursos', [CursoController::class, 'index']);
+Route::post('/curso', [CursoController::class, 'store']);
+Route::get('/curso/{curso}', [CursoController::class, 'show']);
+Route::put('/curso/{curso}', [CursoController::class, 'update']);
+Route::delete('/curso/{curso}', [CursoController::class, 'destroy']);
+
+Route::get('/alunos', [AlunoController::class, 'index']);
+Route::post('/aluno', [AlunoController::class, 'store']);
+Route::get('/aluno/{aluno}', [AlunoController::class, 'show']);
+Route::put('/aluno/{aluno}', [AlunoController::class, 'update']);
+Route::delete('/aluno/{aluno}', [AlunoController::class, 'destroy']);
